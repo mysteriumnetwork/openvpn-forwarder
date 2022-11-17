3580522e920630c5ae7585d3c36bfaa00880bed3
Valdas Petrulis <petrulis.valdas@gmail.com>
2022-11-17 20:27:33 +0200

    Implement E2E test for HTTP traffic

--- ci/commands/command_test_e2e.go
+++ ci/commands/command_test_e2e.go
@@ -18,87 +18,30 @@
 package commands
 
 import (
-	"errors"
 	"fmt"
 
-	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
 
-var (
-	dockerCompose    = sh.RunCmd("docker-compose", "-f", "docker-compose.yml")
-	dockerComposeOut = sh.OutCmd("docker-compose", "-f", "docker-compose.yml")
-	ipTablesAppend   = sh.RunCmd("docker-compose", "exec", "forwarder", "iptables", "-t", "nat", "-A")
-	ipTablesDelete   = sh.RunCmd("docker-compose", "exec", "forwarder", "iptables", "-t", "nat", "-D")
-)
+var dockerCompose = sh.RunCmd("docker-compose", "-f", "docker-compose.yml")
 
 // TestE2E runs end-to-end test
 func TestE2E() (err error) {
 	fmt.Println("Starting E2E containers")
-	if err = dockerCompose("up", "-d"); err != nil {
+	if err = runCompose("up", "-d"); err != nil {
 		return err
 	}
+
 	defer func() {
 		fmt.Println("Forwarder logs:")
 		_ = runCompose("logs", "forwarder")
 		fmt.Println()
 
-		err = dockerCompose("down")
+		err = runCompose("down")
 	}()
 
-	mg.Deps(TestE2EHTTP, TestE2EHTTPS)
-	return nil
-}
-
-// TestE2EHTTPS runs end-to-end test for HTTPS traffic forwarding
-func TestE2EHTTPS() error {
-	originalIP := checkIP("https://api.ipify.org/?format=text")
-	fmt.Println("Original IP:", originalIP)
-
-	redirectRule := []string{"OUTPUT", "-p", "tcp", "-m", "tcp", "--dport", "443", "-j", "REDIRECT", "--to-ports", "8443"}
-	if err := ipTablesAppend(redirectRule...); err != nil {
-		return err
-	}
-	defer ipTablesDelete(redirectRule...)
-
-	currentIP := checkIP("https://api.ipify.org/?format=text")
-	fmt.Println("Current IP:", currentIP)
-
-	if currentIP == originalIP {
-		return errors.New("request proxying failed")
-	}
-	fmt.Printf("Request successfuly proxied: %s -> %s", originalIP, currentIP)
-	return nil
-}
-
-// TestE2EHTTP runs end-to-end test for HTTP traffic forwarding
-func TestE2EHTTP() error {
-	originalIP := checkIP("http://api.ipify.org/?format=text")
-	fmt.Println("Original IP:", originalIP)
-
-	redirectRule := []string{"OUTPUT", "-p", "tcp", "-m", "tcp", "--dport", "80", "-j", "REDIRECT", "--to-ports", "8443"}
-	if err := ipTablesAppend(redirectRule...); err != nil {
-		return err
-	}
-	defer ipTablesDelete(redirectRule...)
-
-	currentIP := checkIP("http://api.ipify.org/?format=text")
-	fmt.Println("Current IP:", currentIP)
-
-	if currentIP == originalIP {
-		return errors.New("request proxying failed")
-	}
-	fmt.Printf("Request successfuly proxied: %s -> %s", originalIP, currentIP)
-	return nil
-}
-
-func checkIP(apiURL string) string {
-	ip, err := dockerComposeOut("exec", "forwarder", "wget", "-q", "-O", "-", apiURL)
-	if err != nil {
-		panic(err)
-	}
-
-	return ip
+	fmt.Println("Starting E2E tests")
+	return runCompose("run", "-T", "machine", "go", "test", "-v", "-tags=e2e", "./e2e/...")
 }
 
 func runCompose(args ...string) error {
